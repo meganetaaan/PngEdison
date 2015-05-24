42862c6294fe9495310658f5bacdd3257bd87ff1
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2015-05-24 14:41:16 +0900

    draw jp font test

---
+++ test.js
@@ -0,0 +1,30 @@
+var sourceStr = process.argv[2];
+var Iconv = require('iconv').Iconv;
+var iconv = new Iconv('UTF-8', 'EUC-JP');
+var kuten2 = function(str){
+    var i;
+    var buf = iconv.convert(str);
+    for(i = 0; i < buf.length; i++){
+        buf.set(i, buf.get(i) - 0xA0);
+    }
+    return buf;
+};
+
+var fs = require('fs'),
+    PNG = require('node-png').PNG;
+// prepare font source
+fs.createReadStream('public/images/misaki8x8.png')
+    .pipe(new PNG())
+    .on('parsed', function () {
+        var str = kuten2(sourceStr);
+        var width = 8;
+        var height = 8;
+        var subImg = new PNG({"width" : width * sourceStr.length, "height" : height});
+
+        for(var i = 0; i < sourceStr.length; i++){
+            var sy = (str[i * 2] - 1) * height;
+            var sx = (str[i * 2 + 1] - 1) * width;
+            this.bitblt(subImg, sx, sy, width, height, i * width, 0);
+        }
+        //subImg.pack().pipe(fs.createWriteStream('out.png'));
+    });
